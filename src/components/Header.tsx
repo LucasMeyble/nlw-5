5dbe1787e6a801ed19df77dcf9ccf37abf9d8a8f
lucas_meyble <lucasmeyble123@gmail.com>
2021-04-22 00:40:39 -0300

    enviroment Button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,8 +26,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        padding: 20,
-        paddingVertical: 20,
+        paddingVertical: 30,
         marginTop: getStatusBarHeight(),
     },
 
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { EnviromentButton } from '../components/EnviromentButton';
 import { Header } from '../components/Header';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
     return(
         <View style={styles.container}>
-            <Header />
+            <View style={ styles.header }>
+                <Header />
+
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subTitle}>
+                    Você quer colocar sua planta? 
+                </Text>
+            </View>
+
+            <EnviromentButton title="Cozinha" active />
         </View>
     )
 }
@@ -18,5 +31,24 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.background,
     },
+
+    header: {
+        paddingHorizontal: 20
+    },
+
+    title: {
+        fontSize: 17,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+
+    subTitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading,
+    }
 
 })
