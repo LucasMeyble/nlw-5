3337abfc03328c3c2facf7e92d85b29180bbc3f7
Lucas Meyble <lucasmeyble123@gmail.com>
2021-04-22 11:44:08 -0300

    butao de lugar

--- src/components/EnviromentButton.tsx
+++ src/components/EnviromentButton.tsx
@@ -33,7 +33,8 @@ const styles = StyleSheet.create({
         height: 40,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 12
+        borderRadius: 12,
+        marginRight: 5
     },
 
     containerActive: {
