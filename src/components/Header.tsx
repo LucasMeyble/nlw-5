d38e05cdb784ac5dd66480f8b682be36507eee4b
Lucas Meyble <lucasmeyble123@gmail.com>
2021-04-23 10:23:17 -0300

    carregando nome de usuario

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import userImg from '../assets/lucas.jpg';
@@ -7,12 +8,22 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Header(){
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageUserName(){
+            const user = await AsyncStorage.getItem('@plantmanager:user')
+            setUserName(user || ' ');
+        }
+
+        loadStorageUserName();
+    }, [])
 
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Olá,</Text>
-                <Text style={styles.userName}>Lucas</Text>
+                <Text style={styles.userName}>{userName}</Text>
             </View>
             <Image source={userImg} style={styles.image}/>
         </View> 
